086a2a42d555a086fb48f0160762be14e05bea69
novo <weixiong_zhu@163.com>
2022-03-01 23:55:39 +0800

    最长递增子序列（贪心 + 二分） 摆动序列（动态规划、贪心）

--- src/day17/LongestIncreasingSubsequence.java
+++ src/day17/LongestIncreasingSubsequence.java
@@ -89,4 +89,52 @@ public class LongestIncreasingSubsequence {
         }
         return res;
     }
+
+    //贪心 + 二分
+    public int lengthOfLIS3(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        // 表示长度为 i 的 所有递增子序列的结尾的最小值 用的不是动态规划数组名随便起的
+        int[] dp = new int[n + 1];
+        // 做一个位置的偏移
+        dp[1] = nums[0];
+        int len = 1;
+        for (int i = 1; i < n; i++) {
+            if (dp[len] < nums[i]) {
+                dp[++len] = nums[i];
+            } else {
+                int index = binarySearch(dp, 1, len, nums[i]);
+                if (index != -1) {
+                    dp[index] = nums[i];
+                }
+            }
+        }
+        return len;
+    }
+
+    private int binarySearch(int[] sortedArr, int L, int R, int target) {
+        if (sortedArr == null || sortedArr.length == 0) {
+            return -1;
+        }
+        int left = L;
+        int right = R;
+        int mid = 1;
+        // 循环条件至少有两个数
+        while (left < right) {
+            mid = left + ((right - left) >> 1);
+            // 因为是严格递增 所以如果target等于dp里的数的话不更新
+            if (sortedArr[mid] == target) {
+                return -1;
+            } else if (sortedArr[mid] < target) {
+                left = mid + 1;
+            } else if (sortedArr[mid] > target){
+                // 为了保证找出的数一定是第一个大于target的，right指针赋值为mid而不是mid - 1
+                // 如果世上mid - 1找出的数只是最邻近target的值，而不能确定这个数比target大还是小
+                right = mid;
+            }
+        }
+        return left;
+    }
 }
