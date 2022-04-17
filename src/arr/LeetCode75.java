00adeb90f4fe682eab14657fc9b2e12ef6d9f61b
novo <weixiong_zhu@163.com>
2022-04-17 22:33:04 +0800

    LeetCode75.颜色分类

---
+++ src/arr/LeetCode75.java
@@ -0,0 +1,55 @@
+package arr;
+
+import java.util.Arrays;
+
+/**
+ * @author novo
+ * @date 2022/4/17-21:48
+ */
+public class LeetCode75 {
+    public void sortColors(int[] nums) {
+        int[] count = new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+        int index = 0;
+        for (int i = 0; i < count[0]; i++) {
+            nums[index++] = 0;
+        }
+        for (int i = 0; i < count[1]; i++) {
+            nums[index++] = 1;
+        }
+        for (int i = 0; i < count[2]; i++) {
+            nums[index++] = 2;
+        }
+    }
+
+    public void sortColors2(int[] nums) {
+        // 维护区间[0, zero]都为0
+        int zero = -1;
+        // 维护区间[two, n - 1]都为2
+        int two = nums.length;
+
+        for (int i = 0; i < two; ) {
+            if (nums[i] == 1) {
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums, --two, i);
+            } else if (nums[i] == 0) {
+                swap(nums, ++zero, i++);
+            }
+        }
+    }
+
+    private void swap (int[] nums, int i, int j) {
+        nums[i] = nums[i] + nums[j];
+        nums[j] = nums[i] - nums[j];
+        nums[i] = nums[i] - nums[j];
+    }
+
+    public static void main(String[] args) {
+        LeetCode75 leetCode75 = new LeetCode75();
+        int[] nums = new int[]{2};
+        leetCode75.sortColors2(nums);
+    }
+}
